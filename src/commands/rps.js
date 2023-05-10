c5c2da0c4dc1326913ebbae7e316fdbae380d5de
ptm119 <normalboy0119@gmail.com>
2023-05-10 21:03:55 +0900

    Fix some bugs

--- src/commands/rps.js
+++ src/commands/rps.js
@@ -34,11 +34,34 @@ module.exports = {
         const player = interaction.options.getString("선택");
         let vs;
         if ((ai === ":fist:" && player === ":raised_hand:") || (ai === ":raised_hand:" && player === ":v:") || (ai === ":v:" && player === ":fist:")) {
-            vs = `${player} **승리!** ${ai}`;
+            vs = `(나)  ${player}  **승리!**  ${ai}  (봇)`;
+            if (user.stack + 1 >= 2) {
+                vs += `\n${user.stack + 1}연승 중!`;
+            }
+            vs += `\n+ ${2 ** (user.stack)}포인트`;
+            vs += `\n현재 포인트 : ${user.point + 2 ** user.stack}`
+            await DB.updateOne({ username: user.username }, { stack: user.stack + 1, point: user.point + 2 ** (user.stack) });
         } else if (ai === player) {
-            vs = `${player} **비김!** ${ai}`;
+            user = await DB.findOne({ username });
+            vs = `(나)  ${player}  **비김!**  ${ai}  (봇)`;
+            if (user.stack >= 2) {
+                vs += "\n연승 깨짐!";
+            }
+            vs += `\n현재 포인트 : ${user.point}`
+            await DB.updateOne({ username: user.username }, { stack: 0 });
         } else {
-            vs = `${player} **패배!** ${ai}`;
+            user = await DB.findOne({ username });
+            vs = `(나)  ${player}  **패배!**  ${ai}  (봇)`;
+            console.log(user);
+            if (user.point !== 0) {
+                vs += `\n- ${user.stack === 0 ? 1 : user.stack}포인트`;
+                vs += `\n현재 포인트 : ${user.point - user.stack === 0 ? 1 : user.stack}`
+                await DB.updateOne({ username: user.username }, { stack: 0, point: (user.point - user.stack === 0) || (user.stack === 0) ? 1 : user.stack });
+            } else {
+                vs += `\n현재 포인트 : ${user.point}`
+                await DB.updateOne({ username: user.username }, { stack: 0 });
+            }
+
         }
         await interaction.reply("가위");
         await wait(250);
